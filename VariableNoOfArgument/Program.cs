770023c5a2748ea81d8931df43e1aaca95cb7fbd
Administrator <abhi_sri_amity6@yahoo.com>
2017-06-18 03:29:58 +0530

    fucntion getmin implemented

--- VariableNoOfArgument/Program.cs
+++ VariableNoOfArgument/Program.cs
@@ -8,7 +8,25 @@ namespace VariableNoOfArgument
 {
     class VariableClass
     {
+        int min;
+        int max;
+        public VariableClass()
+        {
 
+        }
+        public int getmin(params int[] numbers)
+        {
+            min = numbers[0];
+            for (int i=1; i < numbers.Length; i++)
+            {
+
+            if(numbers[i]<min)
+                {
+                    min = numbers[i];
+                }
+            }
+            return min;
+        }
 
 
 
@@ -17,6 +35,18 @@ namespace VariableNoOfArgument
     {
         static void Main(string[] args)
         {
+            VariableClass varclass = new VariableClass();
+            Console.Write("Enter Length of Array : ");
+            int arrarlen = int.Parse(Console.ReadLine());
+            int[] array = new int[arrarlen];
+            for (int i = 0; i < arrarlen; i++)
+            {
+                Console.Write("Enter {0} Element of Array : ", i );
+                int arrelement = int.Parse(Console.ReadLine());
+                array[i] = arrelement;
+            }
+         Console.WriteLine("Minimum Value is {0} ", varclass.getmin(array));
+            Console.Read();
         }
     }
 }
